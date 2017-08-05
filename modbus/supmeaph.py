76a2b891a579379cb7b9fb2ef4495971cd66e2eb
willreddin <willreddin88@gmail.com>
2017-08-05 13:43:27 +0000

    got ph working to write to keen

--- modbus/supmeaph.py
+++ modbus/supmeaph.py
@@ -1,25 +1,22 @@
 import minimalmodbus
 
-class SupmeaPH200(minimalmodbus.Instrument):
-	"""Instrument class for Supmea PH2oo controller
-
-	Args:
-        * portname (str): port name
-        * slaveaddress (int): slave address in the range 1 to 247
-
-    """
-
+class SupmeaPH( minimalmodbus.Instrument):
+    
     def __init__(self, portname, slaveaddress):
-    	minimalmodbus.Instrument.__init__(self, portname, slaveaddress)
-
+        minimalmodbus.Instrument.__init__(self, portname, slaveaddress)
+    
     def get_ph(self):
-    	"""Return PH value"""
-    	return self.read_register(1,2)
-
+        """Return PH value"""
+        return self.read_register(0,0)
+    
     def get_temp(self):
-    	"""Return temp from controller"""
-    	return self.read_register(2,1)
-
+        """Return temp from controller"""
+        return self.read_register(1,0)
+    
     def get_ORP(self):
-    	"""Return ORP value"""
-    	return self.read_register(3,0)
+        """Return ORP value"""
+        return self.read_register(2,0)
+        
+    
+    
+
